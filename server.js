e9c555cb55ca2197772c28cf7e59681a37fd15ec
theChamp <kaifj99@gmail.com>
2018-07-04 21:20:21 +0530

    Heroku ready

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerHelper('getCurrentYear',()=>{
@@ -48,6 +49,6 @@ app.get('/about',(req,res)=>{
 	});
 });
 
-app.listen(3000,()=>{
-	console.log('Server running on port 3000');
+app.listen(port,()=>{
+	console.log(`Server running on port ${port}`);
 });
